615b5734c9d61ed08305322a2078ee7a5e797e29
Alan José <alanjsdelima@gmail.com>
2024-10-30 10:12:13 -0300

    Instatiating and calling the controller

--- public/index.php
+++ public/index.php
@@ -8,6 +8,9 @@
 /*******************************
  * Helpful constants.
  ******************************/
+
+use Core\Http\Router;
+
 const BASE_PATH = __DIR__ . '/../';
 const SERVICE_KEY_PREFIX = '_service_';
 
@@ -15,3 +18,15 @@ const SERVICE_KEY_PREFIX = '_service_';
  * Calling autoload.
  ******************************/
 require BASE_PATH . 'bootstrap/autoload.php';
+
+/**
+ * Application routing.
+ */
+// TODO: Check force https scheme.
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$requestMethod = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+$router = new Router();
+
+require BASE_PATH . 'routes/web.php';
+
+$router->route($requestUri, $requestMethod);
---
+++ routes/web.php
@@ -0,0 +1,3 @@
+<?php
+
+$router->get('/', [\Core\Http\Controllers\HomeController::class, 'home']);
