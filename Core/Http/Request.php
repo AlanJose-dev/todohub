ceeb0e28da0ab0ec5930043d5b836abfdc04a82c
Alan José <alanjsdelima@gmail.com>
2024-10-31 10:59:59 -0300

    Adding Uri class to Request class

--- Core/Http/Request.php
+++ Core/Http/Request.php
@@ -4,20 +4,39 @@ namespace Core\Http;
 
 use Core\Http\Controllers\HeaderBag;
 
+/**
+ * Handle the incoming request.
+ */
 class Request
 {
+    /**
+     * Current request method.
+     * @var string|mixed
+     */
     private string $method;
 
-    // TODO: Create URI Object.
-    private string $uri;
+    /**
+     * Object to handle the current request uri.
+     * @var Uri|array|false|int|string|null
+     */
+    private Uri $uri;
 
+    /**
+     * Stores and handle the request inputs.
+     * @var InputBag
+     */
     private InputBag $inputBag;
 
+    /**
+     * Stores and handle the request headers.
+     * @var HeaderBag
+     */
     private HeaderBag $headerBag;
 
     public function __construct()
     {
         $this->method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+        $this->uri = new Uri(parse_url($_SERVER['REQUEST_URI']));
         $this->uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $this->inputBag = new InputBag($this->method === 'GET' ? $_GET : $_POST);
         $this->headerBag = new HeaderBag(getallheaders());
---
+++ Core/Http/Uri.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Core\Http;
+
+/**
+ * Handle the incoming request ui.
+ */
+class Uri
+{
+    /**
+     * Store the parsed uri.
+     * @var array|false|int|string|null
+     */
+    private array $fragmentedUri = [];
+
+    public function __construct(array $fragmentedUri)
+    {
+        $this->fragmentedUri = $fragmentedUri;
+    }
+
+    /**
+     * Get the fragmented uri.
+     * @return array
+     */
+    public function getFragmentedUri(): array
+    {
+        return $this->fragmentedUri;
+    }
+
+    /**
+     * Get the uri path.
+     * @return string
+     */
+    public function path(): string
+    {
+        return $this->fragmentedUri['path'];
+    }
+
+    /**
+     * Get the uri query.
+     * @return string|null
+     */
+    public function query(): ?string
+    {
+        return $this->fragmentedUri['query'];
+    }
+
+    /**
+     * Get the uri host.
+     * @return string
+     */
+    public function host(): string
+    {
+        return $this->fragmentedUri['host'];
+    }
+
+    /**
+     * Get the uri scheme.
+     * @return string
+     */
+    public function scheme(): string
+    {
+        return $this->fragmentedUri['scheme'];
+    }
+
+    /**
+     * Get the uri port.
+     * @return int
+     */
+    public function port(): int
+    {
+        return $this->fragmentedUri['port'];
+    }
+
+    /**
+     * Get the uri fragment.
+     * @return string|null
+     */
+    public function fragment(): ?string
+    {
+        return $this->fragmentedUri['fragment'];
+    }
+
+    /**
+     * Get the uri password.
+     * @return string|null
+     */
+    public function pass(): ?string
+    {
+        return $this->fragmentedUri['pass'];
+    }
+
+    /**
+     * Get the uri user.
+     * @return string|null
+     */
+    public function user(): ?string
+    {
+        return $this->fragmentedUri['user'];
+    }
+}
