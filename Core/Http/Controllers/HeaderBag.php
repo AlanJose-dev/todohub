39600e7644bede99593507a59c3ac6d150a8a1a4
Alan José <alanjsdelima@gmail.com>
2024-10-31 10:44:51 -0300

    Adding HeaderBag class

---
+++ Core/Http/Controllers/HeaderBag.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Core\Http\Controllers;
+
+use Core\Http\Interfaces\BagInterface;
+
+/**
+ * Handle the request headers.
+ */
+class HeaderBag implements BagInterface
+{
+    /**
+     * Input payload.
+     * @var array
+     */
+    private array $payload = [];
+
+    public function __construct(array $payload)
+    {
+        $this->payload = $payload;
+    }
+
+    /**
+     * Get an item from bag.
+     * @param string $key
+     * @param mixed|null $default
+     * @return mixed
+     */
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->payload[$key] ?? $default;
+    }
+
+    /**
+     * Set a bag item.
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function set(string $key, mixed $value): void
+    {
+        $this->payload[$key] = $value;
+    }
+
+    /**
+     * Checks if the item exists in the bag.
+     * @param string $key
+     * @return bool
+     */
+    public function has(string $key): bool
+    {
+        return array_key_exists($key, $this->payload);
+    }
+
+    /**
+     * Remove an item from bag.
+     * @param string $key
+     * @return void
+     */
+    public function remove(string $key): void
+    {
+        unset($this->payload[$key]);
+    }
+}
