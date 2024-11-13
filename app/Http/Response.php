c1c2beb8d858845191d7b5deeb314fb52e88eccf
Alan José <alanjsdelima@gmail.com>
2024-11-12 22:27:27 -0300

    Adding response class

---
+++ app/Http/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    public static function json(array $payload, int $status = 200, array $headers = []): void
+    {
+        $jsonData = json_encode($payload, JSON_UNESCAPED_UNICODE);
+        header('Content-Type: application/json');
+        foreach ($headers as $header => $value) {
+            header("$header: $value");
+        }
+        http_response_code($status);
+        echo $jsonData;
+    }
+}
