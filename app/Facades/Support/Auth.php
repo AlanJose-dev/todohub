b69cd306047860df9b063ef92cb89dee503a6ded
Alan José <alanjsdelima@gmail.com>
2024-11-26 21:14:37 -0300

    Refactoring authenticate on Auth facade

--- app/Facades/Support/Auth.php
+++ app/Facades/Support/Auth.php
@@ -8,16 +8,17 @@ class Auth
 {
     public static function attempt($email, $password)
     {
-        $statement = DB::connection()->prepare('select id, name, email, password from users where email = :email');
-        $statement->bindParam(':email', $email);
-        $statement->execute();
-        $user = $statement->fetch();
+        $user = User::select([
+            'id', 'name', 'email', 'password'
+        ],
+        [
+            'email.=' => $email
+        ])->first();
 
         if(!$user || !password_verify($password, $user->password)) {
             return false;
         }
 
-        // Replace by __sleep magic method on model.
         unset($user->password);
         self::authenticate($user);
         return true;
@@ -25,19 +26,19 @@ class Auth
 
     public static function authenticate(User $user)
     {
-        $session = app()->resolve('_session');
+        $session = session();
         $session->set('user', $user);
         $session->migrate(true);
     }
 
     public static function check()
     {
-        return app()->resolve('_session')->has('user');
+        return session()->has('user');
     }
 
     public static function user(): ?User
     {
-        return app()->resolve('_session')->get('user');
+        return session()->get('user');
     }
 
     public static function logout()
